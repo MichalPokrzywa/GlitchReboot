04a0a2c52218183c274aca6ece468706a2f500d2
Andrzej Sułkowski <asul449@wp.pl>
2024-12-01 08:58:10 +0100

    level2

--- Assets/StreamingAssets/level_2.lua
+++ Assets/StreamingAssets/level_2.lua
@@ -3,11 +3,8 @@ function While(con)
     CheckResult(not con)
 end
 ----
-if not (platform1 == nil or platform2 == nil or platform3 == nil) then
+if not (x == nil or y == nil or z == nil) then
 --
-x = platform1
-y = platform2
-z = platform3
 While(x + y != z)
 --
 end--
--- Assets/StreamingAssets/level_3.lua
+++ Assets/StreamingAssets/level_3.lua
@@ -3,11 +3,8 @@ function While(con)
     CheckResult(not con)
 end
 ----
-if not (platform1 == nil or platform2 == nil or platform3 == nil) then
+if not (x == nil or y == nil or z == nil) then
 --
-x = platform1
-y = platform2
-z = platform3
 for i = 1, x do
     y = y + i
 end
