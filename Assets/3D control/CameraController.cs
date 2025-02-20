a45f3bac4c54ff235187b8f4fd75c3127ed497da
Ompio <72317376+Ompio@users.noreply.github.com>
2025-02-20 14:04:46 +0100

    Camera Fix
    skrypty od kamery są na fixed update teraz co jest dosyć ważne bo jej prędkość wcześniej zależała od fps, do przyjrzenia się będą jeszcze skrypty korzystające z orientation playera chociaż one nie powinny sprawiać problemów

--- Assets/3D control/CameraController.cs
+++ Assets/3D control/CameraController.cs
@@ -20,7 +20,7 @@ public class CameraControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // if (!UIControl.isPaused)
         // {
