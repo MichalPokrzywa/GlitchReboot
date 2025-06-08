2632ecb1dfea39a23c7f1effc707d9793f232f83
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2025-06-08 22:41:54 +0200

    FirrstGhost

--- Assets/System/ScriptingGame/CSVersion/GhostLevel1.cs
+++ Assets/System/ScriptingGame/CSVersion/GhostLevel1.cs
@@ -8,6 +8,7 @@ public class GhostLevel1 : PuzzleBase
     [Header("PuzzleItems")]
     [SerializeField] private Transform stairsCover;
     [SerializeField] private Transform stairs;
+    [SerializeField] private GameObject floor;
     private Vector3 stairsBasicPosition;
     private Vector3 stairsCoverBasicPosition;
 
@@ -22,6 +23,7 @@ public class GhostLevel1 : PuzzleBase
         if (GetVariableValue<bool>("StairsPlatform"))
         {
             StartCoroutine(SmoothMoveStairs(stairsCover, new Vector3(stairsCoverBasicPosition.x, stairsCoverBasicPosition.y, stairsCoverBasicPosition.z + 12f), 1.5f));
+            StartCoroutine(FloorGlitch(1.6f));
             StartCoroutine(SmoothMoveStairs(stairs, new Vector3(stairsBasicPosition.x, stairsBasicPosition.y + 12f, stairsBasicPosition.z), 1.5f));
         }
     }
@@ -42,5 +44,16 @@ public class GhostLevel1 : PuzzleBase
 
         // Upewniamy siê, ¿e obiekt trafia dok³adnie do docelowej pozycji.
         transform.position = targetPosition;
+    }
+
+    private IEnumerator FloorGlitch( float duration)
+    {
+        floor.GetComponent<GlitchSwitcher>().ApplyGlitch(true);
+        stairs.GetComponent<GlitchSwitcher>().ApplyGlitch(true);
+        stairsCover.GetComponent<GlitchSwitcher>().ApplyGlitch(true);
+        yield return new WaitForSeconds(duration);
+        floor.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
+        stairs.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
+        stairsCover.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
     }
 }
