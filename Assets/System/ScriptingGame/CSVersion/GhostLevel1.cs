5d7521144532d5382aa12ee297434382a9f14352
Womelanczuk544 <72348539+Womelanczuk544@users.noreply.github.com>
2025-06-09 23:43:02 +0200

    ghostlevel work in progress

--- Assets/System/ScriptingGame/CSVersion/GhostLevel1.cs
+++ Assets/System/ScriptingGame/CSVersion/GhostLevel1.cs
@@ -8,23 +8,49 @@ public class GhostLevel1 : PuzzleBase
     [Header("PuzzleItems")]
     [SerializeField] private Transform stairsCover;
     [SerializeField] private Transform stairs;
+    [SerializeField] private Transform bridge;
     [SerializeField] private GameObject floor;
     private Vector3 stairsBasicPosition;
     private Vector3 stairsCoverBasicPosition;
+    private Vector3 bridgeBasicPosition;
+    private bool stairsState = false;
+    private bool bridgeState = false;
+    private float duration = 6f;
 
     public void Awake()
     {
-        stairsBasicPosition = stairs.transform.position;
-        stairsCoverBasicPosition = stairsCover.transform.position;
+        stairsBasicPosition = stairs.position;
+        stairsCoverBasicPosition = stairsCover.position;
+        bridgeBasicPosition = bridge.position;
     }
 
     public override void DoTerminalCode()
     {
         if (GetVariableValue<bool>("StairsPlatform"))
         {
-            StartCoroutine(SmoothMoveStairs(stairsCover, new Vector3(stairsCoverBasicPosition.x, stairsCoverBasicPosition.y, stairsCoverBasicPosition.z + 12f), 1.5f));
-            StartCoroutine(FloorGlitch(1.6f));
-            StartCoroutine(SmoothMoveStairs(stairs, new Vector3(stairsBasicPosition.x, stairsBasicPosition.y + 12f, stairsBasicPosition.z), 1.5f));
+            stairsState = true;
+            StartCoroutine(SmoothMoveStairs(stairsCover, new Vector3(stairsCoverBasicPosition.x, stairsCoverBasicPosition.y, stairsCoverBasicPosition.z + 12f), duration));
+            StartCoroutine(FloorGlitch(duration+0.2f));
+            StartCoroutine(SmoothMoveStairs(stairs, new Vector3(stairsBasicPosition.x, stairsBasicPosition.y + 12f, stairsBasicPosition.z), duration));
+        }
+        else if (GetVariableValue<bool>("BridgePlatform"))
+        {
+            bridgeState = true;
+            StartCoroutine(SmoothMoveStairs(bridge, new Vector3(bridgeBasicPosition.x, bridgeBasicPosition.y, bridgeBasicPosition.z + 18f), duration));
+            StartCoroutine(BridgeGlitch(duration + 0.2f));
+        }
+        else if (!GetVariableValue<bool>("StairsPlatform") && stairsState)
+        {
+            stairsState = false;
+            StartCoroutine(SmoothMoveStairs(stairsCover, new Vector3(stairsCoverBasicPosition.x, stairsCoverBasicPosition.y, stairsCoverBasicPosition.z ), duration));
+            StartCoroutine(FloorGlitch(duration + 0.2f));
+            StartCoroutine(SmoothMoveStairs(stairs, new Vector3(stairsBasicPosition.x, stairsBasicPosition.y , stairsBasicPosition.z), duration));
+        }
+        else if (!GetVariableValue<bool>("BridgePlatform") && bridgeState)
+        {
+            bridgeState = false;
+            StartCoroutine(SmoothMoveStairs(bridge, new Vector3(bridgeBasicPosition.x, bridgeBasicPosition.y, bridgeBasicPosition.z ), duration));
+            StartCoroutine(BridgeGlitch(duration + 0.2f));
         }
     }
 
@@ -55,5 +81,12 @@ public class GhostLevel1 : PuzzleBase
         floor.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
         stairs.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
         stairsCover.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
+    }
+
+    private IEnumerator BridgeGlitch(float duration)
+    {
+        bridge.GetComponent<GlitchSwitcher>().ApplyGlitch(true);
+        yield return new WaitForSeconds(duration);
+        bridge.GetComponent<GlitchSwitcher>().ApplyGlitch(false);
     }
 }
