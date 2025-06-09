66d2feaa7aa3015017cccecb6e431c1d92e9280b
keskese449 <asul449@wp.pl>
2025-06-09 03:12:18 +0200

    ochrona przed spadaniem v1

---
+++ Assets/scripts/RepositionScript.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class RepositionScript : MonoBehaviour
+{
+    public GameObject rePosition;
+    
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (rePosition != null && collision.gameObject.CompareTag("Player")) 
+        { 
+            collision.gameObject.transform.position = rePosition.transform.position;
+        }
+    }
+}
